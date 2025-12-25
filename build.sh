945c08975c7bd04ecdbeac19a607059038570d82
fcurrk <48197714+fcurrk@users.noreply.github.com>
2025-12-25 12:21:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,8 +26,8 @@ elif [ "$1" = "beta" ]; then
   webVersion="rolling"
 else
   git tag -d beta
-  version="4.1.18.251219"
-  webVersion="4.1.18.251219"
+  version="4.1.18.251225"
+  webVersion="4.1.18.251225"
 fi
 
 echo "backend version: $version"
